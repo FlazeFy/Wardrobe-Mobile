01ec7ebec419ede24f8891797b7d88ec556e49f3
Leonardho Rante S <flazen.edu@gmail.com>
2025-03-21 13:50:22 +0700

    [Mobile] Init Firebase FCM, Crashlytics, and Performance

--- wardrobe/lib/design_tokens/global.dart
+++ wardrobe/lib/design_tokens/global.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 int selectedIndex = 0;
 
 bool isOffline = false;
@@ -5,3 +7,13 @@ bool isOffline = false;
 // API Fetch rest time
 int clothesHeaderFetchRestTime = 60; // in second
 int statsFetchRestTime = 180;
+
+// Firebase
+String? token;
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel',
+  'High Importance Notifications',
+  description: 'This channel is used for important notifications.',
+  importance: Importance.high,
+);
--- wardrobe/lib/main.dart
+++ wardrobe/lib/main.dart
@@ -1,10 +1,50 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:wardrobe/design_tokens/global.dart';
 import 'package:wardrobe/design_tokens/style.dart';
 import 'package:wardrobe/modules/sqlite/init.dart';
 import 'package:wardrobe/organisms/o_bottom_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() async {
+bool shouldUseFirestoreEmulator = false;
+
+Future<void> fireFCMHandler(RemoteMessage message) async {
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseMessaging.onBackgroundMessage(fireFCMHandler);
+  await FlutterLocalNotificationsPlugin()
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  //FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   WidgetsFlutterBinding.ensureInitialized();
   final DatabaseHelper dbHelper = DatabaseHelper();
 
@@ -12,15 +52,86 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    FlutterLocalNotificationsPlugin().initialize(initializationSettings);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        FlutterLocalNotificationsPlugin().show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                color: primaryColor,
+                enableLights: true,
+                icon: "@mipmap/ic_launcher",
+              ),
+            ));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: Text(notification.title ?? ""),
+              content: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text(notification.body ?? "")],
+                ),
+              ),
+            );
+          },
+        );
+      }
+    });
+
+    getToken();
+  }
+
+  getToken() async {
+    token = await FirebaseMessaging.instance.getToken();
+
+    print(token);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      title: 'Flutter Demo',
-      home: OrganismsBottomBar(),
+    //Lock device on portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Wardrobe',
+      home: const OrganismsBottomBar(),
     );
   }
 }
